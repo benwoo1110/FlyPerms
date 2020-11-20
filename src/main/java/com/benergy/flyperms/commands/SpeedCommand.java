134444df5b70b40d4238da1507cf008540d684c6
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2020-11-20 14:44:46 +0800

    Starting implementation of changing other's speed.

--- src/main/java/com/benergy/flyperms/commands/SpeedCommand.java
+++ src/main/java/com/benergy/flyperms/commands/SpeedCommand.java
@@ -1,14 +1,11 @@
 package com.benergy.flyperms.commands;
 
-import co.aikar.commands.annotation.CommandAlias;
-import co.aikar.commands.annotation.CommandPermission;
-import co.aikar.commands.annotation.Description;
-import co.aikar.commands.annotation.Subcommand;
-import co.aikar.commands.annotation.Syntax;
+import co.aikar.commands.annotation.*;
 import com.benergy.flyperms.FlyPerms;
 import com.benergy.flyperms.enums.Commands;
 import com.benergy.flyperms.enums.Permissions;
 import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandAlias(Commands.BASE)
@@ -23,11 +20,24 @@ public class SpeedCommand extends FlyPermsCommand {
     @Syntax("<speed>")
     @Description("Changes fly speed, from -10 to 10.")
     public void onSpeed(Player player, int speed) {
-        if (!this.plugin.getSpeedChecker().canChangeSpeedTo(player, speed)) {
-            player.sendMessage(ChatColor.RED + "You are not allowed to set fly to this speed!");
+        changeSpeed(player, player, speed);
+    }
+
+    @Subcommand(Commands.SPEED)
+    @CommandPermission(Permissions.CHANGE_SPEED_OTHERS)
+    @CommandCompletion(" @players")
+    @Syntax("<speed> [player]")
+    @Description("Displays ability to fly for another player.")
+    public void onSpeedOther(CommandSender sender, int speed, Player targetPlayer) {
+        changeSpeed(sender, targetPlayer, speed);
+    }
+
+    private void changeSpeed(CommandSender sender, Player targetPlayer, int speed) {
+        if (!this.plugin.getSpeedChecker().canChangeSpeedTo(targetPlayer, speed)) {
+            sender.sendMessage(ChatColor.RED + "You are not allowed to set fly to this speed!");
             return;
         }
-        player.setFlySpeed((float) speed / 10);
-        player.sendMessage("Successfully set flying speed to " + speed);
+        targetPlayer.setFlySpeed((float) speed / 10);
+        sender.sendMessage("Successfully set flying speed to " + speed);
     }
 }
