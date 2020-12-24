f2ec8c84bf5469781d8c5888209a2a0ed40f53f9
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2020-12-24 23:37:33 +0800

    Add docs to FPCheckManager api.

--- src/main/java/com/benergy/flyperms/api/FPCheckManager.java
+++ src/main/java/com/benergy/flyperms/api/FPCheckManager.java
@@ -4,6 +4,9 @@ import com.benergy.flyperms.Constants.FlyState;
 import com.benergy.flyperms.checkers.GameModeChecker;
 import com.benergy.flyperms.checkers.SpeedChecker;
 import com.benergy.flyperms.checkers.WorldChecker;
+import com.benergy.flyperms.utils.SpeedGroup;
+import org.bukkit.GameMode;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 
 /**
@@ -11,6 +14,7 @@ import org.bukkit.entity.Player;
  */
 public interface FPCheckManager {
     /**
+     * Derive the {@link FlyState} the target is currently in.
      *
      * @param player A bukkit {@link Player} entity.
      * @return The {@link FlyState} that player should be in.
@@ -18,6 +22,7 @@ public interface FPCheckManager {
     FlyState calculateFlyState(Player player);
 
     /**
+     * Change speed for a given player, if allowed based on {@link SpeedGroup} permissions they have.
      *
      * @param player A bukkit {@link Player} entity.
      * @param speed  Target fly speed the player want to change to.
@@ -26,20 +31,23 @@ public interface FPCheckManager {
     boolean canChangeSpeedTo(Player player, double speed);
 
     /**
+     * Gets the {@link SpeedChecker}.
      *
-     * @return {@link Checker} for {@link com.benergy.flyperms.utils.SpeedGroup} object.
+     * @return {@link Checker} for {@link SpeedGroup} object.
      */
     SpeedChecker getSpeedChecker();
 
     /**
+     * Gets the {@link WorldChecker}.
      *
-     * @return {@link PlayerChecker} for {@link org.bukkit.World} object.
+     * @return {@link PlayerChecker} for {@link World} object.
      */
     WorldChecker getWorldChecker();
 
     /**
+     * Gets the {@link GameModeChecker}.
      *
-     * @return {@link PlayerChecker} for {@link org.bukkit.GameMode} object.
+     * @return {@link PlayerChecker} for {@link GameMode} object.
      */
     GameModeChecker getGameModeChecker();
 }
