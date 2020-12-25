f3a20a08f4ed637178987a120f47a42acb5a234e
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2020-12-25 17:07:02 +0800

    test jenkins build (again)

--- src/main/java/dev/benergy10/flyperms/commands/FlyPermsCommand.java
+++ src/main/java/dev/benergy10/flyperms/commands/FlyPermsCommand.java
@@ -4,9 +4,9 @@ import co.aikar.commands.BaseCommand;
 import dev.benergy10.flyperms.FlyPerms;
 
 public abstract class FlyPermsCommand extends BaseCommand {
-
-    protected final FlyPerms plugin;
     
+    protected final FlyPerms plugin;
+
     protected FlyPermsCommand(FlyPerms plugin) {
         this.plugin = plugin;
     }
