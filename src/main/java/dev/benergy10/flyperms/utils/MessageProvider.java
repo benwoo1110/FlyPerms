f4faffc72c1b2b0c4f0dbdebe6a0b9e4902dc8e1
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2021-02-06 23:35:53 +0800

    Implement color parsing to MessageProvider.

--- src/main/java/dev/benergy10/flyperms/utils/MessageProvider.java
+++ src/main/java/dev/benergy10/flyperms/utils/MessageProvider.java
@@ -2,6 +2,7 @@ package dev.benergy10.flyperms.utils;
 
 import dev.benergy10.flyperms.Constants.MessageKey;
 import dev.benergy10.flyperms.FlyPerms;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.YamlConfiguration;
 
@@ -13,12 +14,13 @@ import java.util.Map;
 
 public class MessageProvider {
 
+    private static final char COLOUR_CHAR = '&';
     private static final String MESSAGE_FILENAME = "messages.yml";
-    private static final String INVALID_MESSAGE = "!!INVALID!!";
+    private static final String INVALID_MESSAGE = "&c!!INVALID!!";
 
     private final FlyPerms plugin;
     private Map<String, String> defaultMessagesMap;
-    private Map<String, String> messagesMap;
+    private Map<String, String> customMessagesMap;
 
     public MessageProvider(FlyPerms plugin) {
         this.plugin = plugin;
@@ -26,6 +28,7 @@ public class MessageProvider {
     }
 
     private void loadDefault() {
+        Logging.debug("Loading default locale...");
         this.defaultMessagesMap = new HashMap<>(MessageKey.values().length);
 
         InputStream messageFileStream = this.plugin.getResource(MESSAGE_FILENAME);
@@ -36,18 +39,25 @@ public class MessageProvider {
 
         Reader messageFileReader = new InputStreamReader(messageFileStream);
         YamlConfiguration messageYaml = YamlConfiguration.loadConfiguration(messageFileReader);
-
-        Logging.debug("Loading default locale...");
-
-        for (MessageKey messageKey : MessageKey.values()) {
-            this.defaultMessagesMap.put(messageKey.name(), messageYaml.getString(messageKey.name(), INVALID_MESSAGE));
-            Logging.debug(String.valueOf(messageKey));
-            Logging.debug(String.valueOf(this.defaultMessagesMap));
-        }
+        populateMessagesMap(this.defaultMessagesMap, messageYaml);
     }
 
     public void load() {
         // this.messagesMap = new HashMap<>(Messages.values().length);
+    }
+
+    private void populateMessagesMap(Map<String, String> messagesMap, YamlConfiguration messageYaml) {
+        for (MessageKey messageKey : MessageKey.values()) {
+            messagesMap.put(
+                    messageKey.name(),
+                    colourise(messageYaml.getString(messageKey.name(), INVALID_MESSAGE))
+            );
+        }
+        Logging.debug(String.valueOf(messagesMap));
+    }
+
+    private String colourise(String message) {
+        return ChatColor.translateAlternateColorCodes(COLOUR_CHAR, message);
     }
 
     public String getMessage(MessageKey messageKey) {
